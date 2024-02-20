919385c742e13e2b92c6e56db12e180b458c25a7
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-02-20 15:21:01 +0200

    Add Friend component to be more dynamic

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,3 +1,35 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 export default function FriendsList() {
-  return <ul>List</ul>;
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return <li>{friend.name}</li>;
 }
