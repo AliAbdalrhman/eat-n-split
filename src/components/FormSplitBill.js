39566d1bd18cf145954839f1ddfddade6b88d862
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-02-22 13:01:48 +0200

    Add split form

---
+++ src/components/FormSplitBill.js
@@ -0,0 +1,26 @@
+import Button from "./Button";
+
+export default function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill wit X</h2>
+
+      <label>💰Bill Value</label>
+      <input type="text" />
+
+      <label>🚶‍♂️Your expense</label>
+      <input type="text" />
+
+      <label>👭Bill Value</label>
+      <input type="text" disabled />
+
+      <label>🤑Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
