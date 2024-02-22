3d40513ac0e1e9b6764800e185db61abbc1944ff
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-02-22 12:43:15 +0200

    Add Form Add Friend component to our js files

---
+++ src/components/FormAddFriend.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+export default function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👨🏼‍🤝‍👨🏼Friend name</label>
+      <input type="text" />
+
+      <label>🧑Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
