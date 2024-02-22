acda8672bf6fb60806a0578a231aa650d1130dac
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-02-22 12:46:06 +0200

    Replace button element with Button compoent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import FriendsList from "./components/FriendsList";
+import FormAddFriend from "./components/FormAddFriend";
+import Button from "./components/Button";
 
 export default function App() {
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
     </div>
   );
