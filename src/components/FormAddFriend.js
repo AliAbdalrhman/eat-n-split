3a20a3a511d3c8881ac871aab407415e2f15ea13
Ali Abdalrhman <ali.abdalrhman.606@gmail.com>
2024-02-23 14:54:15 +0200

    Add a functionality of adding name and image of new friends

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
   return (
     <form className="form-add-friend">
       <label>👨🏼‍🤝‍👨🏼Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🧑Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
